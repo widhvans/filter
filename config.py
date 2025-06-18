472e73eb5469c60c124e9f20d0c95deb3e1921db
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-18 15:13:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 
 # अपना टेलीग्राम बॉट टोकन यहाँ पेस्ट करें
 # यह टोकन आपको BotFather से मिलेगा।
-TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+TELEGRAM_BOT_TOKEN = "7320891454:AAHp3AAIZK2RKIkWyYIByB_fSEq9Xuk9-bk"
